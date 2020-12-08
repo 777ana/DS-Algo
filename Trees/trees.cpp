1a7acff506c65bbc40bf86497de75f1f5d5b976e
Anand <anandabhinav888@gmail.com>
2020-12-09 00:35:50 +0530

    commit on master

--- Trees/trees.cpp
+++ Trees/trees.cpp
@@ -23,6 +23,18 @@ class TreeNode {
     }
 };
 
+template <typename T>
+class PairClass{
+    public:
+    TreeNode<int>* largest;
+    TreeNode<int>* secondLargest;
+
+    PairClass(TreeNode<int>* first, TreeNode<int>* second){
+        this->largest=first;
+        this->secondLargest=second;
+    }
+};
+
 // Function for taking Levelwise input of the tree
 TreeNode<int>* takeInputLevelwise(){
     int rootData;
@@ -63,7 +75,7 @@ TreeNode<int>* takeInputLevelwise(){
 
 }
 
-// Function for taking input of the tree
+//1 Function for taking input of the tree
 TreeNode<int>* takeInput(){
     int rootData;
     cout<<"Enter data:"<<endl;
@@ -81,7 +93,7 @@ TreeNode<int>* takeInput(){
 }
 
 
-// Function for printing the tree
+//2 Function for printing the tree
 void printTree(TreeNode<int>* root){
     if (root == NULL)
     {
@@ -102,7 +114,7 @@ void printTree(TreeNode<int>* root){
 }
 
 
-// Function for printing the tree levelwise
+//3 Function for printing the tree levelwise
 void printTreeLevelwise(TreeNode<int>* root){
     if (root == NULL)
     {
@@ -136,7 +148,7 @@ void printTreeLevelwise(TreeNode<int>* root){
     
 }
 
-// node counter using queue
+//4 node counter using queue
 int countNodes(TreeNode<int>* root){
     int counter=1;
     queue<TreeNode<int>*> q;
@@ -155,7 +167,7 @@ int countNodes(TreeNode<int>* root){
     
 }
 
-// node counter using recursion
+//5 node counter using recursion
 int countNodesRec(TreeNode<int>* root){
     int ans=1;
     for (int i = 0; i < root->children.size(); i++)
@@ -165,7 +177,7 @@ int countNodesRec(TreeNode<int>* root){
     return ans;
 }
 
-// finding the sum of all the nodes
+//6 finding the sum of all the nodes
 int sumOfNodes(TreeNode<int>* root){
     if (root ==NULL)
     {
@@ -180,7 +192,7 @@ int sumOfNodes(TreeNode<int>* root){
     return sumNodes;
 }
 
-// find node with max value
+//7 find node with max value
 TreeNode<int>* maxNode(TreeNode<int>* root){
     if (root == NULL)
     {
@@ -198,7 +210,7 @@ TreeNode<int>* maxNode(TreeNode<int>* root){
     return maxi;
 }
 
-// find height of the tree
+//8 find height of the tree
 /*
 root k child p ja aur child ka height dekhle bas, jaha p max height aa raha hoga waha p 
 return karrwale height and add +1 to it as we are considering height of root is 1.
@@ -221,7 +233,7 @@ int heightOfTree(TreeNode<int>* root){
     return 1+height;
 }
 
-// print nodes till kth Depth from the root
+//9 print nodes till kth Depth from the root
 void printdDepthKthNode(TreeNode<int>* root, int k){
     if (k==0)
     {
@@ -234,7 +246,7 @@ void printdDepthKthNode(TreeNode<int>* root, int k){
     }
 }
 
-// number of leaf nodes in the tree
+//10 number of leaf nodes in the tree
 int noOfLeafNodes(TreeNode<int>* root){
     if (root==NULL)
     {
@@ -252,7 +264,7 @@ int noOfLeafNodes(TreeNode<int>* root){
     return leaf;
 }
 
-// print tree in preorder, means pehle root phirr uska baccha
+//11 print tree in preorder, means pehle root phirr uska baccha
 void printPreOrder(TreeNode<int>* root){
     if (root==NULL)
     {
@@ -265,7 +277,7 @@ void printPreOrder(TreeNode<int>* root){
     }
 }
 
-// printing like pehle baccha phirr root
+//12 printing like pehle baccha phirr root
 void printPostOrder(TreeNode<int>* root){
     if (root==NULL)
     {
@@ -278,7 +290,7 @@ void printPostOrder(TreeNode<int>* root){
     cout<<root->data<<' '; 
 }
 
-// Find and return the node with next larger element in the Tree
+//13 Find and return the node with next larger element in the Tree
 TreeNode<int>* nextLargestEle(TreeNode<int>* root, int n){
     TreeNode<int>* ans=NULL;
     if (root==NULL)
@@ -305,6 +317,107 @@ TreeNode<int>* nextLargestEle(TreeNode<int>* root, int n){
     return ans;
 }
 
+void helperForreplaceWithDepthValue(TreeNode<int>* root, int depth){
+    root->data=depth;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        helperForreplaceWithDepthValue(root->children[i], depth+1);
+    }
+    
+}
+//14 In a given Generic Tree, replace each node with its depth value.
+void replaceWithDepthValue(TreeNode<int> *root){
+    if (root==NULL)
+    {
+        return;
+    }
+    helperForreplaceWithDepthValue(root, 0);   
+}
+
+/*15
+Given a tree, find and return the node for which sum of data of all children and 
+the node itself is maximum. In the sum, data of node itself and data of immediate 
+children is to be taken.
+*/
+void maxSumNodeHelper(TreeNode<int>* root, TreeNode<int>** resNode, int* maxsum){
+    if (root == NULL)
+    {
+        return;
+    }
+    int currsum=root->data;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        currsum+=root->children[i]->data;
+        maxSumNodeHelper(root->children[i], resNode, maxsum);
+    }
+    if (currsum>*maxsum)
+    {
+        *resNode=root;
+        *maxsum=currsum;
+    }
+    return;
+}
+
+TreeNode<int>* maxSumNode(TreeNode<int>* root){
+    TreeNode<int>* resNode;
+    int maxsum=0;
+    maxSumNodeHelper(root, &resNode, &maxsum);
+    return resNode;
+}
+
+/* 16
+find and return the node with second largest value in given tree. 
+Return NULL if no node with required value is present.
+*/
+
+PairClass<int>* secondLargestNodeHelper(TreeNode<int>* root){
+    if (root==NULL)
+    {
+        PairClass<int>* ans= new PairClass<int>(NULL, NULL);
+        return ans;
+    }
+    PairClass<int>* ans=new PairClass<int>(root, NULL);
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        PairClass<int>* childAns=secondLargestNodeHelper(root->children[i]);
+        if (childAns->largest->data>ans->largest->data)
+        {   
+            if (childAns->secondLargest==NULL)
+            {
+                ans->secondLargest=ans->largest;
+                ans->largest=childAns->largest;
+            }
+            else
+            {
+                if (childAns->secondLargest->data>ans->largest->data)
+                {
+                    ans->secondLargest=childAns->secondLargest;
+                    ans->largest=childAns->largest;
+                }
+                else
+                {
+                    ans->secondLargest=ans->largest;
+                    ans->largest=childAns->largest;
+                }
+            }
+        }
+        else if (ans->largest->data==childAns->largest->data && childAns->secondLargest!=NULL)
+        {
+            ans->secondLargest=childAns->secondLargest;
+        }
+        else if (ans->largest->data!=childAns->largest->data && ans->secondLargest==NULL 
+        || childAns->largest->data>ans->secondLargest->data)
+        {
+            ans->secondLargest=childAns->largest;
+        }
+    }
+    return ans;
+}
+
+TreeNode<int>* secondLargestNode(TreeNode<int>* root){
+    return secondLargestNodeHelper(root)->secondLargest;
+
+}
 
 int main(){
     /*
@@ -325,7 +438,7 @@ int main(){
     cout<< "Printing nodes till 2nd Depth: "<<endl;
     printdDepthKthNode(root, 2);
     cout<<endl;
-    // 1 3 2 3 4 2 5 6 2 7 8 0 0 0 0 1 9 0
+    /* 1 3 2 3 4 2 5 6 2 7 8 0 0 0 0 1 9 0 */
     cout<<"No. of leaf nodes in the tree: "<<noOfLeafNodes(root)<<endl;
     cout<<"Pre-order traversal of tree : "<<endl;
     printPreOrder(root);
@@ -333,16 +446,21 @@ int main(){
     cout<<"Post-order traversal of tree : "<<endl;
     printPostOrder(root);
     cout<<endl;
-    if (nextLargestEle(root, 9)!=NULL)
+    if (nextLargestEle(root, 7)!=NULL)
     {
-        cout<<"Next largest ele for 8 in tree is :"<<nextLargestEle(root, 9)->data<<endl;
+        cout<<"Next largest ele for 7 in tree is :"<<nextLargestEle(root, 7)->data<<endl;
     }
     else
     {
         cout<<"Next largest ele for 8 in tree is -"<<INT_MIN<<endl;
     }
+    // cout<<"Replacing tree with its depth"<<endl;
+    // replaceWithDepthValue(root);
+    // printTreeLevelwise(root);
+    cout<<"the node for which sum of data of all children is:"<<maxSumNode(root)->data<<endl;
+    cout<<"2nd largest ele of the tree is- "<<secondLargestNode(root)->data<<endl;
     delete root;
-    cout<<endl;
+    // cout<<endl;
     cout<<"Tree deleted"<<endl;
     // return 0;
 }
